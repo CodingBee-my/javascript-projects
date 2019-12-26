4ce5d4a744745950f19c3bcda66f85e40db4a05c
yuan wei <yuanwei92@gmail.com>
2019-12-26 12:49:30 +0800

    use displayFact to dispay error message

--- Number Facts/main.js
+++ Number Facts/main.js
@@ -1,13 +1,16 @@
-// Create 
+// Create variables to hold the HTML element of number input and fact display
 const factTextElement = document.querySelector("#fact");
 const numberInputElement = document.querySelector("#numberInput");
 
 function onGetFactClicked() {
   let number = numberInputElement.value;
 
-  // Check if number is empty
+  //
+  // Check if number is empty, if so display
+  // a message asking user to put in any number
+  //
   if (number == "") {
-    factTextElement.innerHTML = "Please put in any number!";
+    displayFact("Please put in any number!");
     return
   }
 
